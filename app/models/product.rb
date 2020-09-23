8693aa110ef11aa84b06a89743211c3fdfe2ae3b
vagrant <vagrant@localhost.localdomain>
2020-09-23 05:05:42 +0000

    first commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,13 @@ class Product < ApplicationRecord
   has_many :order_products, dependent: :destroy
   has_many :orders, through: :order_products, dependent: :destroy
   enum sale_status: { 販売中: 0, 売切れ: 1}
+
+
+  def self.search(search)
+	if search
+   	  @product = Product.where("name LIKE?","%#{search}%")
+	else
+	  Product.all
+	end
+  end
 end
